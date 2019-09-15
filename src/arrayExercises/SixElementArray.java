a44177f86da08b87fd81297b2e6129d7e4dc9fb9
Alexandru <triboi.alexandru@gmail.com>
2019-09-15 21:17:06 +0300

    Array elements

---
+++ src/arrayExercises/SixElementArray.java
@@ -0,0 +1,12 @@
+package arrayExercises;
+
+import java.util.Scanner;
+
+public class SixElementArray {
+    public static void main(String[] args) {
+
+        int[] f = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(f[5]);
+
+    }
+}
